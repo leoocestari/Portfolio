14c3d5e25f15ca5408e0c93e6c1b83f709740dc5
leoocestari <leonardocestari67@gmail.com>
2024-04-09 16:18:09 -0300

    contato att

--- src/app/components/contato/index.jsx
+++ src/app/components/contato/index.jsx
@@ -1,6 +1,9 @@
 import Styles from "./contato.module.scss"
 import Image from "next/image"
 import Pessoa from "/public/images/pessoa4.svg"
+import Button from "../button"
+import Githicon from "/public/images/githicon.svg"
+import Linkicon from "/public/images/linkicon.svg"
 
 const Contato = () => {
   return(
@@ -8,18 +11,41 @@ const Contato = () => {
     <div className={Styles.header}>
       <h1>Contato</h1>
     </div>
-    <div className={Styles.image}>
-    <Image src={Pessoa} alt="fundo"
-    width={450}/>
+    <div className={Styles.abacontato}>
+    <form className={Styles.formulario}>
+      <input required placeholder="Digite seu nome" type="text" class="nome">
+      </input>
+      <input required placeholder="Digite seu email" type="email" class="emailclass">
+      </input>
+      <input required placeholder="Digite o assunto" type="text" class="assuntoo">
+      </input>
+      <textarea required placeholder="Sua mensagem..." cols={30} rows={10} class="textarea"></textarea>
+      <Button title="Enviar"/>
+      
+    </form>
+    <div className={Styles.contatoadicional}>
+      <h1>E-mail para contato</h1>
+      <p1>leonardocestari67@gmail.com</p1>
+      <h1>Redes Sociais</h1>
+      <div className={Styles.redes}>
+      <div className={Styles.gitrede}>
+      <div className={Styles.boxicon}>
+      <Image src={Githicon}
+      width={30}></Image>
+      </div>
+      <p>/leoocestari</p>
+      </div>
+      <div className={Styles.linkrede}>
+      <div className={Styles.boxicon2}>
+      <Image src={Linkicon}
+      width={30}></Image>
+      </div>
+      <p>/leoocestari</p>
+      </div>
+      </div>
+    </div>
+    </div>
     
-    </div>
-    <div className={Styles.text}>
-    <p>1
-    </p>
-    <p>
-      1
-    </p>
-    </div>
   </div>
   );
 };
