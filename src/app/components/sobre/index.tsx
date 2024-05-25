b9c0757a135377522783ee0c94d67092f9fde6a1
leoocestari <leonardocestari67@gmail.com>
2024-05-25 00:34:27 -0300

    about att

--- src/app/components/sobre/index.tsx
+++ src/app/components/sobre/index.tsx
@@ -1,6 +1,11 @@
 import Styles from "./sobre.module.scss"
 import Image from "next/image"
 import Pessoa from "/public/images/newilu.svg"
+import Icon1 from "/public/images/nextblack.svg"
+import Icon2 from "/public/images/ANGULARblack2.svg"
+import Icon3 from "/public/images/reactblack.svg"
+import Icon4 from "/public/images/typeblack.svg"
+import Icon5 from "/public/images/sassblack2.svg"
 
 
 function Sobre () {
@@ -23,7 +28,42 @@ function Sobre () {
       Atualmente cursando análise e desenvolvimento de sistemas e focando em
       adquirir experiencia na área.
     </p>
+    <div className={Styles.boxicon}>
+      <Image
+      alt="icon1" src={Icon1}
+      width={32}
+      height={40}
+      className="invert">
+      </Image>
+      <Image
+      alt="icon1" src={Icon2}
+      width={40}
+      height={40}
+      className="invert">
+      </Image>
+      <Image
+      alt="icon1" src={Icon3}
+      width={40}
+      height={40}
+      className="invert">
+      </Image>
+      <Image
+      alt="icon1" src={Icon4}
+      width={33}
+      height={40}
+      className="invert">
+      </Image>
+      <Image
+      alt="icon1" src={Icon5}
+      width={40}
+      height={40}
+      className="invert">
+      </Image>
+      </div>
+      
+      
     </div>
+    
   </div>
   );
 };
